d2854adf702866a35ae823a18c25e6c4ec3aa4a7
Alex <login@sneaksneak.org>
2018-05-08 10:59:44 -0700

    Do not prompt for confirmation during ubuntu bootstrapw

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -4,13 +4,13 @@
 # Update package manager
 ################################################
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 #################################################
 # install python and ansible
 #################################################
 
-sudo apt install python python-pip
-pip install --upgrade pip
-if [ ! -f /usr/local/bin/ansible ]; then pip install ansible; fi 
+sudo apt install python python-pip -y
+sudo pip install --upgrade pip
+if [ ! -f /usr/local/bin/ansible ]; then sudo pip install ansible; fi
